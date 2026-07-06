180437127217258075aef2cf5d8988a2910da204
Ponce <gustavogd271@gmail.com>
2026-07-06 01:49:53 -0600

    Confirmar acceso web de paciente 7

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ if __name__ == "__main__":
     main()
 
     #Set-Content -Path "data\history.json" -Value "[]" -Encoding UTF8 Pa borrar papi analisis y historial....
+#https://neuro-space-creaj.onrender.com
