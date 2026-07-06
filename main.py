fc8a1a1a94df470086982dadca4f0e88a5a6a2a5
Ponce <gustavogd271@gmail.com>
2026-07-06 10:37:02 -0600

    Actualizar pacientes

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     #Set-Content -Path "data\history.json" -Value "[]" -Encoding UTF8 Pa borrar papi analisis y historial....
 #https://neuro-space-creaj.onrender.com
 #python web_server/server.py
+
 #git add .
-#git commit -m "Actualizar expedientes de prueba"
+#it commit -m "Actualizar expedientes de prueba"
 #git push
