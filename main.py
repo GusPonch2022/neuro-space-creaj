076cfe6991e7dd98aa63f4db956d1960b5e14898
Ponce <gustavogd271@gmail.com>
2026-07-10 07:43:24 -0600

    Actualizar pacientes

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
 
     #Set-Content -Path "data\history.json" -Value "[]" -Encoding UTF8 Pa borrar papi analisis y historial....
 #https://neuro-space-creaj.onrender.com
-#python web_server/server.py
+#python web_server/server.py                        Activar el servidor
 
-#git add .
-#git commit -m "Actualizar pacientes"
+#git add .                               
+#git commit -m "Actualizar pacientes"                Sera para actualizar el Codigo qr por ahora.....
 #git push
